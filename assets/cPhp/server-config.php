ee2bb102f7f57e3b012349afd0cd10a7286e6320
brainaxe1995 <108518951+brainaxe1995@users.noreply.github.com>
2025-06-09 11:17:26 +0400

    Update server-config.php

--- assets/cPhp/server-config.php
+++ assets/cPhp/server-config.php
@@ -2,16 +2,9 @@
 $environment = 'local'; // 'local' or 'live'
 
 $config = [
-    'local' => ['base_url' => 'http://localhost/portal'],
-    'live'  => ['base_url' => 'https://portal.tootfunyachts.com'],
+  'local' => ['base_url' => 'http://localhost/portal'],
+  'live'  => ['base_url' => 'https://portal.tootfunyachts.com'],
 ];
 
-// Allow overriding the base URL using an environment variable
-$envBaseUrl = getenv('PROJECT_BASE_URL');
-
-$baseUrl = ($envBaseUrl !== false && $envBaseUrl !== '')
-    ? $envBaseUrl
-    : $config[$environment]['base_url'];
-
-define('PROJECT_BASE_URL', $baseUrl);
+define('PROJECT_BASE_URL', $config[$environment]['base_url']);
 ?>
